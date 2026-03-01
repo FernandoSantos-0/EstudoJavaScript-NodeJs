f26ed6a0dbfdf655de5bac6e88532c370fd2ab7f
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2026-03-01 11:07:01 -0300

    Log 0.002

---
+++ Java Script/Aula1/ConceitosBasico.js
@@ -0,0 +1,45 @@
+// Variavel
+
+let idade = 10;
+console.log("Olá Mundo!!!");
+console.log(idade);
+
+let idadePessoa = 50;
+let alturaPessoa = 180;
+let nomePessoa = "Jhoan";
+
+console.log(nomePessoa, " Altura: ", alturaPessoa, " Idade: ");
+
+// Constantes
+
+let valor1 = 30;
+valor1 = 100; // Valor pode ser alterado.
+console.log(valor1);
+
+const valor2 = 20; // valor nao pode ser alterado.
+console.log(valor2);
+
+// Tipos Primitivos
+
+    // Tipos de referencia
+
+    let nome1 = "rafael"; // String literal
+    let idade3 = 20.5; // number literal
+    let verdadeOuFalso = true; // boolean
+    let valor6; // undefined
+    let redefinirValor = null; // redefinir valor
+
+// Objetos
+
+let nome;    
+
+let pessoa = {
+    
+    nome: 'João',
+    idade: 50,
+    alturaPessoa: 2.0,
+
+};
+
+console.log(pessoa.alturaPessoa,pessoa.idade,pessoa.nome)
+
