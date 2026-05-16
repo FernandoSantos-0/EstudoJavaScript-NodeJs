70abf93fcec3112d17ced653e220e768bea9b938
Fernando Pereira Dos Santos <fp354575@gmail.com>
2026-05-16 14:28:16 -0300

    exercicio js

---
+++ javascript/exercicios/exercicios/exercicio01/script.js
@@ -0,0 +1,14 @@
+// desafio 1
+
+let nomeLutado = 'joao paulo';
+let nacionalidade = 'russo';
+let idade = 44;
+
+
+document.write()
+
+document.write(`
+    Nome ${nomeLutado} <br> 
+    Nacionalidade: ${nacionalidade} <br>
+    Idade: ${idade} anos <br>
+    `);
